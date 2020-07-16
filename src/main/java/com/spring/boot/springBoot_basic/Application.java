5245e345a550325661e1c1bb9dd396af256a198d
jhn5052 <jhn5052@naver.com>
2020-07-16 15:30:04 +0900

    use lombok

---
+++ src/main/java/com/spring/boot/springBoot_basic/Application.java
@@ -0,0 +1,10 @@
+package com.spring.boot.springBoot_basic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
