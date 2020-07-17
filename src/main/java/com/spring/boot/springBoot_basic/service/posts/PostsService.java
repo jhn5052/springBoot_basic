aecf88ccdae2f74b63696d46002f4ad7e5cb3fd8
jhn5052 <jhn5052@naver.com>
2020-07-17 15:56:27 +0900

    머스테치를 이용한 게시판 제작 실습(등록, 수정, 삭제 기능)

--- src/main/java/com/spring/boot/springBoot_basic/service/posts/PostsService.java
+++ src/main/java/com/spring/boot/springBoot_basic/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.spring.boot.springBoot_basic.service.posts;
 
 import com.spring.boot.springBoot_basic.domain.posts.Posts;
 import com.spring.boot.springBoot_basic.domain.posts.PostsRepository;
+import com.spring.boot.springBoot_basic.web.dto.PostsListResponseDto;
 import com.spring.boot.springBoot_basic.web.dto.PostsResponseDto;
 import com.spring.boot.springBoot_basic.web.dto.PostsSaveRequestDto;
 import com.spring.boot.springBoot_basic.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,10 +30,24 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
+    }
 
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAll().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
 }
--- src/test/java/com/spring/boot/springBoot_basic/domain/PostRepositoryTest.java
+++ src/test/java/com/spring/boot/springBoot_basic/domain/PostRepositoryTest.java
@@ -1,5 +1,7 @@
-package com.spring.boot.springBoot_basic.domain.posts;
+package com.spring.boot.springBoot_basic.domain;
 
+import com.spring.boot.springBoot_basic.domain.posts.Posts;
+import com.spring.boot.springBoot_basic.domain.posts.PostsRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -39,5 +43,26 @@ public class  PostRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
 
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2020,7,17,12,57,0);
+        postsRepository.save(Posts.builder()
+        .title("title")
+        .content("content")
+        .author("author")
+        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>> createDate = " + posts.getCreateDate()+ ", modifiedDate = " + posts.getModifiedDate());
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
